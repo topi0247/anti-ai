8e46cf1eb545f5ce04ee3acdca3af8792c34f4e9
topi0247 <karubona.ra247@gmail.com>
2025-03-23 14:36:14 +0900

    feat: ロゴ追加

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react"
+import Logo from "./ANTI.png"
 
 function App() {
   const [count, setCount] = useState(0);
@@ -14,10 +15,11 @@ function App() {
   }, []);
 
   return (
-    <main className="flex flex-col items-center justify-center min-h-screen bg-gray-100 text-gray-800">
-      <h1 className="text-4xl font-bold text-red-600 mb-4">
+    <main className="flex flex-col items-center justify-start min-h-screen bg-white text-gray-800">
+      <h1><img src={Logo} alt="logo" width={500} className="w-100 object-cover" /></h1>
+      <h2 className="text-4xl font-bold text-red-600 mb-4">
         AIなんぞ…使ってんじゃねええ！！！！！
-      </h1>
+      </h2>
       <p className="text-lg mb-2">あなたが生成AIサイトへアクセスした回数</p>
       <p className="text-5xl font-semibold text-blue-500">{count}</p>
     </main>
